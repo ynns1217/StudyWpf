8b5ac7e7fb309de23fa7e6c1b499204d8a44fcb8
ynns1217 <ynns1217@naver.com>
2022-06-07 17:42:59 +0900

    220607

---
+++ Day 05/WpfCaliburnApp/WpfCaliburnApp/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Caliburn.Micro;
+using System.Windows;
+using WpfCaliburnApp.ViewModels;
+
+namespace WpfCaliburnApp
+{
+
+    //시작 윈도우 지정하기 위한 클래스
+    class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //base.OnStartup(sender, e);
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ Day 05/WpfCaliburnApp/WpfCaliburnApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Caliburn.Micro;
+
+namespace WpfCaliburnApp.ViewModels
+{
+    class MainViewModel : Screen
+    {
+    }
+}
